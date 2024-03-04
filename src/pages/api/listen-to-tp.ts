46ba4a2a6385bf5f7e540d7f13573b749db89fa3
Juan Flores <juan.flores@insureon.com>
2024-03-04 09:10:30 -0500

    parsing eventdata

--- src/pages/api/listen-to-tp.ts
+++ src/pages/api/listen-to-tp.ts
@@ -9,7 +9,7 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  console.log("[request-body]", req.body);
+  console.log("[request-body]", req.body?.events[0]?.eventData);
   console.log("[request-query]", req.query);
   res.status(200).json({ name: "John Doe" });
 }
